200b28d0f8462142fe0951ddabb6d5e58e4a2141
ka <ka.kaosf@gmail.com>
2017-07-09 12:04:16 +0900

    Add "null: false" limitations

--- db/migrate/20170709030349_create_entries.rb
+++ db/migrate/20170709030349_create_entries.rb
@@ -1,8 +1,8 @@
 class CreateEntries < ActiveRecord::Migration[5.1]
   def change
     create_table :entries do |t|
-      t.references :user, foreign_key: true
-      t.string :body
+      t.references :user, foreign_key: true, null: false
+      t.string :body, null: false
 
       t.timestamps
     end
