ff03810ad9b61d3ea5ebfd96aa9b180bed17d7d2
ka <ka.kaosf@gmail.com>
2017-07-09 17:49:08 +0900

    Create other N+1 (N^2+N+1 ?) Query problem example code

--- a.rb
+++ a.rb
@@ -1,5 +1,11 @@
 User.destroy_all
 Entry.destroy_all
+Comment.destroy_all
 u1 = User.create name: 'User 1'
+u2 = User.create name: 'User 2'
 e1 = Entry.create user: u1, body: 'Entry 1 of u1'
 e2 = Entry.create user: u1, body: 'Entry 2 of u1'
+c1 = Comment.create user: u1, entry: e1, body: 'Comment 1 to e1 of u1'
+c2 = Comment.create user: u2, entry: e1, body: 'Comment 2 to e1 of u2'
+c3 = Comment.create user: u1, entry: e2, body: 'Comment 3 to e2 of u1'
+c4 = Comment.create user: u2, entry: e2, body: 'Comment 4 to e2 of u2'
--- b.rb
+++ b.rb
@@ -4,3 +4,32 @@ Entry.all.each { |e| puts [e.body, e.user.name] }
 Entry.all.includes(:user).each { |e| puts [e.body, e.user.name] }
 # 1 query
 Entry.all.includes(:user).references(:user).each { |e| puts [e.body, e.user.name] }
+
+# N^2 + N + 1 Query...?
+Entry.all.each do |e|
+  puts [e.body, e.user.name]
+  e.comments.each do |c|
+    puts [c.body, c.user.name]
+  end
+end
+# N+1 Query
+Entry.all.includes(:user).each do |e|
+  puts [e.body, e.user.name]
+  e.comments.each do |c|
+    puts [c.body, c.user.name]
+  end
+end
+# N+1 Query (comment's user)
+Entry.all.includes(:user, :comments).each do |e|
+  puts [e.body, e.user.name]
+  e.comments.each do |c|
+    puts [c.body, c.user.name]
+  end
+end
+# 4 queries
+Entry.all.includes(:user, comments: :user).each do |e|
+  puts [e.body, e.user.name]
+  e.comments.each do |c|
+    puts [c.body, c.user.name]
+  end
+end
